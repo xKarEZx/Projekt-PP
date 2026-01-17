0ea94832f02ef6b7e73fca74c89e475e2e29712b
xKarEZx <maciejkara23@gmail.com>
2026-01-17 13:24:36 +0100

    Update mech.h

--- mech.h
+++ mech.h
@@ -1,3 +1,17 @@
+#ifndef MECH_H
+#define MECH_H
 
-erwrwerw
-erwrwerw
+#define MAX_MODEL 100
+#define MAX_KLASA 30
+#define MAX_PILOT 50
+#define MAX_STAN 30
+
+typedef struct {
+    char model[MAX_MODEL];      // unikalny identyfikator
+    char klasa[MAX_KLASA];
+    int moc_reaktora;            // <100
+    char pilot[MAX_PILOT];       // mo¿e byæ pusty
+    char stan[MAX_STAN];         // sprawny/uszkodzony/w naprawie/demonta¿
+} Mech;
+
+#endif
