70d4d44069eb54fc7f117b2a0ceafe498bf3534e
xKarEZx <maciejkara23@gmail.com>
2026-01-20 19:53:30 +0100

    Update fileio.c

--- fileio.c
+++ fileio.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fileio.h"
 
 int saveToFile(Database *db, const char *filename) {
@@ -7,6 +8,7 @@ int saveToFile(Database *db, const char *filename) {
 
     fwrite(&db->size, sizeof(int), 1, f);
     fwrite(db->data, sizeof(Mech), db->size, f);
+
     fclose(f);
     return 1;
 }
@@ -16,10 +18,12 @@ int loadFromFile(Database *db, const char *filename) {
     if (!f) return 0;
 
     fread(&db->size, sizeof(int), 1, f);
+
     db->capacity = db->size + 10;
     db->data = realloc(db->data, sizeof(Mech) * db->capacity);
+
     fread(db->data, sizeof(Mech), db->size, f);
+
     fclose(f);
     return 1;
 }
-
